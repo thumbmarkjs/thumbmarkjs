2df930592b8d7774af0778afdfd0984f08924f10
Ilkka Peltola <ilkka.peltola@zervant.com>
2024-01-05 15:52:39 +0200

    fix media queries for multi-screen setups

--- src/components/screen/screen.ts
+++ src/components/screen/screen.ts
@@ -22,34 +22,20 @@ function matchMedias(): string[] {
 
     const mediaQueries: { [k: string]: string[] } = {
         'prefers-contrast': ['high', 'more', 'low', 'less', 'forced', 'no-preference'],
-        'color-gamut': ['rec2020', 'p3', 'srgb'],
-        'dynamic-range': ['high', 'standard'],
-        'video-dynamic-range': ['high', 'standard'],
         'any-hover': ['hover', 'none'],
         'any-pointer': ['none', 'coarse', 'fine'],
         'pointer': ['none', 'coarse', 'fine'],
         'hover': ['hover', 'none'],
         'update': ['fast', 'slow'],
-        'overflow-block': ['scroll', 'none', 'optional-paged', 'paged'],
-        'overflow-inline': ['scroll', 'none'],
         'color': ['8', '16', '256'],
         'inverted-colors': ['inverted', 'none'],
         'prefers-reduced-motion': ['reduce', 'no-preference'],
         'prefers-reduced-transparency': ['reduce', 'no-preference'],
-        'grid': ['0', '1'],
         'scripting': ['none', 'initial-only', 'enabled'],
         'forced-colors': ['active', 'none'],
         'display-mode': ['fullscreen', 'standalone', 'minimal-ui', 'browser'],
-//        'aspect-ratio': ['1/1', '16/9', '16/10', '4/3'],
-//        'resolution': ['300dpi', '2dppx', '3dppx'],
         'prefers-color-scheme': ['dark', 'light', 'no-preference'],
-//        'overflow': ['auto', 'hidden'],
-//        'transform-3d': ['0', '1'],
-//        'device-aspect-ratio': ['1/1', '16/9', '16/10', '4/3'],
-//        'device-height': ['640px', '768px', '1024px'],
-//        'device-width': ['320px', '360px', '375px'],
-//        'forced-color-adjust': ['none', 'auto'],
-};
+      };
 
     Object.keys(mediaQueries).forEach((key) => {
         mediaQueries[key].forEach((value) => {
