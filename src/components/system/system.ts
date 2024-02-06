84187e65852763af663ce1bd5769365e8c8487fd
Ilkka <ilkkapel@gmail.com>
2024-02-06 10:16:37 +0200

    add applePay and memory heap (#17)
    * add google Pay and memory heap
    
    * move applepay to system
    
    ---------
    
    Co-authored-by: Ilkka Peltola <ilkka.peltola@zervant.com>

--- src/components/system/system.ts
+++ src/components/system/system.ts
@@ -10,9 +10,34 @@ function getSystemDetails(): Promise<componentInterface> {
         'productSub': navigator.productSub,
         'product': navigator.product,
         'useragent': navigator.userAgent,
-        'browser': {'name': browser.name, 'version': browser.version }
+        'browser': {'name': browser.name, 'version': browser.version },
+        'applePayInfo': getApplePayInfo()
     });
 });
 }
 
+/**
+ * @returns applePayCanMakePayments: boolean, applePayMaxSupportedVersion: number
+ */
+function getApplePayInfo(): { applePayCanMakePayments: boolean, applePayMaxSupportedVersion: number } {
+    let applePayCanMakePayments = false;
+    let applePayMaxSupportedVersion = 0;
+
+    if (typeof (window as any).ApplePaySession === 'function') {
+        const versionCheck = (window as any).ApplePaySession.supportsVersion;
+        let version = 0;
+        while (versionCheck(version + 1)) {
+            version++;
+        }
+        applePayCanMakePayments = version > 0;
+        applePayMaxSupportedVersion = version;
+    }
+
+    return {
+        applePayCanMakePayments,
+        applePayMaxSupportedVersion
+    };
+}
+
 includeComponent('system', getSystemDetails);
+
