5a567ae7bc8dd12d5a38ddf8389b1961095e707f
Ilkka Peltola <ilkka.peltola@zervant.com>
2024-02-06 10:11:47 +0200

    add google Pay and memory heap

--- src/components/hardware/hardware.ts
+++ src/components/hardware/hardware.ts
@@ -3,11 +3,14 @@ import { componentInterface, includeComponent } from '../../factory'
 function getHardwareInfo(): Promise<componentInterface> {
   return new Promise((resolve, reject) => {
     const deviceMemory = (navigator.deviceMemory !== undefined) ? navigator.deviceMemory : 0
+    const memoryInfo = (window.performance && (window.performance as any).memory ) ? (window.performance as any).memory : 0
     resolve(
       {
         'videocard': getVideoCard(),
         'architecture': getArchitecture(),
-        'deviceMemory': deviceMemory.toString() || 'undefined'
+        'deviceMemory': deviceMemory.toString() || 'undefined',
+        'jsHeapSizeLimit': memoryInfo.jsHeapSizeLimit || 'undefined',
+        'applePayInfo': getApplePayInfo()
       }
     )
   });
@@ -20,6 +23,7 @@ interface VideoCard {
   
 /**
  * @see Credits: https://stackoverflow.com/a/49267844
+ * @returns VideoCard | "undefined"
  */
 function getVideoCard(): componentInterface | string {
   const canvas = document.createElement('canvas')
@@ -41,5 +45,28 @@ function getArchitecture(): number {
 
   return u8[3];
 }
+/**
+ * @returns applePayCanMakePayments: boolean, applePayMaxSupportedVersion: number
+ */
+function getApplePayInfo(): { applePayCanMakePayments: boolean, applePayMaxSupportedVersion: number } {
+    let applePayCanMakePayments = false;
+    let applePayMaxSupportedVersion = 0;
+
+    if (typeof (window as any).ApplePaySession === 'function') {
+        const versionCheck = (window as any).ApplePaySession.supportsVersion;
+        let version = 0;
+        while (versionCheck(version + 1)) {
+            version++;
+        }
+        applePayCanMakePayments = version > 0;
+        applePayMaxSupportedVersion = version;
+    }
+
+    return {
+        applePayCanMakePayments,
+        applePayMaxSupportedVersion
+    };
+}
+
 
 includeComponent('hardware', getHardwareInfo);
