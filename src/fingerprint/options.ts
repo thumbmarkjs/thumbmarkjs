1a8ff94a46749134652e34ebae648f7d302417d2
Daniele Mancini <daniel.mancini91@gmail.com>
2024-04-14 12:50:33 +0200

    more options (#40)
    * more options
    
    * unmasked vendor and render

--- src/fingerprint/options.ts
+++ src/fingerprint/options.ts
@@ -3,6 +3,9 @@ export interface optionsInterface {
     include?: string[],
     webgl_runs?: number,
     canvas_runs?: number,
+    permissions_to_check?: PermissionName[], // new option
+    retries?: number, // new option
+    timeout?: number, // new option
 }
 
 export let options: optionsInterface = {
@@ -10,9 +13,11 @@ export let options: optionsInterface = {
 }
 
 export function setOption<K extends keyof optionsInterface>(key: K, value: optionsInterface[K]) {
-    if (!['exclude'].includes(key))
+    if (!['exclude', 'permissions_to_check', 'retries', 'timeout'].includes(key))
         throw new Error('Unknown option ' + key)
-    if (['exclude'].includes(key) && !(Array.isArray(value) && value.every(item => typeof item === 'string')) )
-        throw new Error('The value of the exclude must be an array of strings');
+    if (['exclude', 'permissions_to_check'].includes(key) && !(Array.isArray(value) && value.every(item => typeof item === 'string')) )
+        throw new Error('The value of the exclude and permissions_to_check must be an array of strings');
+    if ([ 'retries', 'timeout'].includes(key) && typeof value !== 'number')
+        throw new Error('The value of retries must be a number');
     options[key] = value;
 }
