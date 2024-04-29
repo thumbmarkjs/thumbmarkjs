f22c908e6a4ece5cd88dfac253eec625ac3a8973
Arian-Hamdi <arian.hamdi@gmail.com>
2024-04-29 10:27:51 +0330

    fix: layout shift when adding iframe

--- src/utils/ephemeralIFrame.ts
+++ src/utils/ephemeralIFrame.ts
@@ -7,6 +7,7 @@ export async function ephemeralIFrame(callback: ({ iframe }: { iframe: Document 
     iframe.setAttribute('frameBorder', '0')
 
     const style = iframe.style
+    style.setProperty('position','fixed');
     style.setProperty('display', 'block', 'important')
     style.setProperty('visibility', 'visible')
     style.setProperty('border', '0');
